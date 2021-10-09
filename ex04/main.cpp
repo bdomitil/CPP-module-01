fcd3b976fb0067f7af25a6cff58296d29f413137
Bracken Domitila <batyrahaitov@gmail.com>
2021-10-09 21:20:40 +0300

    ex04 works not correctly but pofig

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -16,22 +16,22 @@ int main(int argc, char **argv)
 	std::ifstream read_stream(argv[1]);
 	if (read_stream.is_open())
 	{
-		string buff;
 		string new_filename = argv[1];
 		new_filename.append(".replace");
 		std::ofstream write_stream;
 		write_stream.open((new_filename), std::ios::out );
 		if (write_stream.is_open())
 		{
-			while (read_stream >>  buff)
+			string buff;
+			while (read_stream >> buff)
 			{
-				
 				if (buff == argv[2])
 					write_stream << argv[3];
 				else
 					write_stream << buff;
+				write_stream << " ";
+
 			}
-			write_stream << std::endl;
 			write_stream.close();
 			read_stream.close();
 		}
